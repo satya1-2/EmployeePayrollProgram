5bc452995706ade8726ef9a0d084f27161d8b908
satya <satya143@gmail.com>
2023-05-09 13:18:14 +0530

    Add:Calculating wages for month assuming 20 working days using for loops

--- JSProgrammingConstructs/EmoplyeePayrollProgram/EmployeePayrollProgram.js
+++ JSProgrammingConstructs/EmoplyeePayrollProgram/EmployeePayrollProgram.js
@@ -1,30 +1,33 @@
 console.log("welcome to payroll services");
 
-// uc2
+// uc4
 const IS_PART_TIME=1;
 const IS_FULL_TIME=2;
 const IS_NO_TIME= 0;
 const PART_TIME_HOURS=4;
 const FULL_TIME_HOURS=8;
-const WAGE_PER_HOURS=20;
+const WAGE_PER_HOUR=20;
+const NUMBER_OF_WORKING_DAYS=20;
 
 function getWorkingHours(empCheck){
 
   switch(empCheck){
         case IS_PART_TIME :
-        empHrs=PART_TIME_HOURS
-        break;
+        return PART_TIME_HOURS;
         case IS_FULL_TIME :
-        empHrs=FULL_TIME_HOURS
-        break;
+        return FULL_TIME_HOURS;
        default:
             return 0;
             
   }
 }
   let empHrs=0;
-  let empCheck=Math.floor(Math.random() *10) %3;
-  empHrs=getWorkingHours(empCheck);
- let employeeWage=empHrs*WAGE_PER_HOURS;
- console.log(" emp daily wage: " + employeeWage);
+  for(let day=0;day<NUMBER_OF_WORKING_DAYS;day++){
+    let empCheck=Math.floor(Math.random() *10) %3;
+    empHrs+=getWorkingHours(empCheck);
+
+  }
+  
+ let employeeWage=empHrs*WAGE_PER_HOUR;
+ console.log("Total hours: " +empHrs+"Employee Daily Wage:"+ employeeWage);
 
