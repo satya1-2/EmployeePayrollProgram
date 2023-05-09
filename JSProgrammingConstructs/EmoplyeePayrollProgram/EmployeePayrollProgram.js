6fe7736a65e348369efba28733f6f01b8c629644
satya <satya143@gmail.com>
2023-05-09 13:00:31 +0530

    Add:write a function to get the work hours

--- JSProgrammingConstructs/EmoplyeePayrollProgram/EmployeePayrollProgram.js
+++ JSProgrammingConstructs/EmoplyeePayrollProgram/EmployeePayrollProgram.js
@@ -7,8 +7,9 @@ const IS_NO_TIME= 0;
 const PART_TIME_HOURS=4;
 const FULL_TIME_HOURS=8;
 const WAGE_PER_HOURS=20;
-let empHrs=0;
-let empCheck=Math.floor(Math.random()*10) %3;
+
+function getWorkingHours(empCheck){
+
   switch(empCheck){
         case IS_PART_TIME :
         empHrs=PART_TIME_HOURS
@@ -16,12 +17,14 @@ let empCheck=Math.floor(Math.random()*10) %3;
         case IS_FULL_TIME :
         empHrs=FULL_TIME_HOURS
         break;
-      
-        default:empHrs=IS_NO_TIME;
-            empHrs=0;
-            empHrs=4;
+       default:
+            return 0;
+            
   }
-
-        let employeeWage=empHrs*WAGE_PER_HOURS;
-        console.log(" emp wage: " + employeeWage);
+}
+  let empHrs=0;
+  let empCheck=Math.floor(Math.random() *10) %3;
+  empHrs=getWorkingHours(empCheck);
+ let employeeWage=empHrs*WAGE_PER_HOURS;
+ console.log(" emp daily wage: " + employeeWage);
 
