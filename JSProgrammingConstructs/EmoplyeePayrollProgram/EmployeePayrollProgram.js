d3d562223684ecea50b2f1334ee6b6d78f0427a1
satya <satya143@gmail.com>
2023-05-09 10:54:20 +0530

    Add:Employee present or Absent

---
+++ JSProgrammingConstructs/EmoplyeePayrollProgram/EmployeePayrollProgram.js
@@ -0,0 +1,14 @@
+console.log("welcome to payroll services");
+
+// uc1
+const IS_Absent=0;
+let empCheck=Math.floor(Math.random()*10)%2;
+if(empCheck=IS_Absent){
+    console.log("Employee is present");
+    return;
+
+}
+else{
+    console.log("Employee is present");
+    return;
+}
