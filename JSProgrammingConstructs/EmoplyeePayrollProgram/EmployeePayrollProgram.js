520d9deccc6c4a911baaed3014d4d8344a5e6b98
satya <satya143@gmail.com>
2023-05-09 12:47:13 +0530

    Add:Calculating employee wage based on part time and full time work

--- JSProgrammingConstructs/EmoplyeePayrollProgram/EmployeePayrollProgram.js
+++ JSProgrammingConstructs/EmoplyeePayrollProgram/EmployeePayrollProgram.js
@@ -1,14 +1,27 @@
 console.log("welcome to payroll services");
 
-// uc1
-const IS_Absent=0;
-let empCheck=Math.floor(Math.random()*10)%2;
-if(empCheck=IS_Absent){
-    console.log("Employee is present");
-    return;
+// uc2
+const IS_PART_TIME=1;
+const IS_FULL_TIME=2;
+const IS_NO_TIME= 0;
+const PART_TIME_HOURS=4;
+const FULL_TIME_HOURS=8;
+const WAGE_PER_HOURS=20;
+let empHrs=0;
+let empCheck=Math.floor(Math.random()*10) %3;
+  switch(empCheck){
+        case IS_PART_TIME :
+        empHrs=PART_TIME_HOURS
+        break;
+        case IS_FULL_TIME :
+        empHrs=FULL_TIME_HOURS
+        break;
+      
+        default:empHrs=IS_NO_TIME;
+            empHrs=0;
+            empHrs=4;
+  }
 
-}
-else{
-    console.log("Employee is present");
-    return;
-}
+        let employeeWage=empHrs*WAGE_PER_HOURS;
+        console.log(" emp wage: " + employeeWage);
+
