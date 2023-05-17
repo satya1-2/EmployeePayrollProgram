29d4a9ff774bc8f5e1acd8f20a1a24b0bdc599af
satya <satya143@gmail.com>
2023-05-17 09:20:15 +0530

    Add:extending data in payroll gender And date

--- EmployeePayrollData.js
+++ EmployeePayrollData.js
@@ -3,12 +3,16 @@ class EmployeePayrollData{
     //id;
     //name;
     //salary;
+    gender;
+    startDate;
 
     //constructor
-    constructor(id,name,salary){
-        this.id=id;
-        this.name=name;
-        this.salary=salary;
+    constructor(...params){
+        this.id=params[0];
+        this.name=params[1];
+        this.salary=params[2];
+        this.gender=params[3];
+        this.startDate=params[4];
     }
 
     //getter and setter methods
@@ -33,7 +37,11 @@ class EmployeePayrollData{
 
     //method
     toString(){
-        return " id = " +this.id + " name = " + this.name + " salary = " + this.salary;
+        const options = { year: 'numeric', month: 'long', day:'numeric'};
+        const empDate = this.startDate === undefined ? "undefined" :
+        this.startDate.toLocaleDateString("en-US", options);
+
+        return "id=" +this.id + " ,name=" + this.name + ",salary=" + this.salary + " gender=" +this.gender + ",start Date=" +empDate;
     }
 
 }
@@ -44,3 +52,5 @@ employeePayrollData.id = 2;
 employeePayrollData.name = "kundan";
 employeePayrollData.salary = 50000;
 console.log(employeePayrollData.toString());
+let newEmployeePayrollData = new EmployeePayrollData(1,"Satya",45000,"M",new Date());
+console.log(newEmployeePayrollData.toString());
